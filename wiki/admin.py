df1a11711c2bc3b9f7bbecd6eeae731e0f459894
Vivian Macedo <vivianmacedo@gmail.com>
2019-01-06 16:40:45 +0100

    Improve admin layout

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Page, Tag
 
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("name", "show_link",)
+    search_fields = ("tags__text",)
+    filter_horizontal = ("tags",)
+
+    def show_link(self, obj):
+        return mark_safe('<a target=blank href="%s">%s</a>' % (obj.link, obj.link))
+
+
+admin.site.register(Page, PageAdmin)
 admin.site.register(Tag)
--- wiki/models.py
+++ wiki/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Tag(models.Model):
     text = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.text
+
 
 class Page(models.Model):
     name = models.CharField(max_length=250)
     link = models.URLField()
     summary = models.TextField()
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
