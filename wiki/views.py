9252a361b2b5dbd35cba8db1728c729e066ee392
Vivian Macedo <vivianmacedo@gmail.com>
2019-01-28 21:23:45 +0100

    Add pagination

--- wiki/views.py
+++ wiki/views.py
@@ -6,13 +6,15 @@ from wiki.models import Page
 class PageListView(ListView):
 
     model = Page
+    paginate_by = 10
 
     def get_queryset(self):
         tags = self.request.GET['tags'].replace(" ","").split(',')
         queryset = super().get_queryset()
         for tag in tags:
             queryset = queryset.filter(tags__text__icontains=tag)
-        return queryset[:30]
+        return queryset
+    
 
 class SearchView(TemplateView):
      template_name = "wiki/search.html"
