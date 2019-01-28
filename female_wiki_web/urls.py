5e9d05d7a5a8fa65149c365bf58f767bc9cc8fe3
Vivian Macedo <vivianmacedo@gmail.com>
2019-01-28 21:34:16 +0100

    Add redirect index to search

--- female_wiki_web/urls.py
+++ female_wiki_web/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('search'), permanent=True)),
     path('wiki/', include('wiki.urls')),
     path('admin/', admin.site.urls),
 ]
